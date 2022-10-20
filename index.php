64954962f8adbcee235635e80583cdc0212c94b7
Vladislav Priske <55079623+wemitwqw@users.noreply.github.com>
2022-10-20 13:28:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Raintree Junior Dev</title>
-  <meta http-equiv = "refresh" content = "0; url = https://www.windboat.ee/eng" />
 </head>
 <body>
 </body>
